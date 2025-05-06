da1342492b27c33442109e3aa7ccf984bfcabb03
abel24m <abel24m@utexas.edu>
2025-05-06 12:15:08 -0500

    Trasnferred api to its own project from react app

---
+++ api.py
@@ -0,0 +1,17 @@
+import json
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/mlb_data')
+def get_current_time():
+    with open ("web_scrape/data_storage/covers_mlb_data_05-06-2025.json") as file:
+        data = json.load(file)
+    return data
+
+@app.route('/nba')
+def get_nba_games():
+    with open("test.txt", "r") as f:
+        content = f.readline()
+    return {'data': content}
