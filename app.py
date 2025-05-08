686556c7e65373a49ce0f8b257073df50814abb9
Abel I Moreno <abel24m@utexas.edu>
2025-05-08 14:47:05 -0500

    temp change for today

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ import schedule
 from flask import Flask
 from pathlib import Path
 
-from .web_scrape.web_scraper import initiate_web_scrape
-
+# from web_scrape import web_scraper
 app = Flask(__name__)
 
 today_date = datetime.now().strftime("%m-%d-%Y")
@@ -15,11 +14,11 @@ yesterday_date = (datetime.now() - timedelta(days=1)).strftime("%m-%d-%Y")
 
 
 # Every Monday task() is called at 20:00
-schedule.every().day.at('10:00').do(initiate_web_scrape)
+# schedule.every().day.at('10:00').do(web_scraper.initiate_web_scrape())
 
 @app.route('/mlb_data')
 def get_current_time():
-    schedule.run_pending()
+    # schedule.run_pending()
     today_path = f"web_scrape/data_storage/covers_mlb_data_{today_date}.json"
     yesterday_path = f"web_scrape/data_storage/covers_mlb_data_{yesterday_date}.json"
     file = Path(today_path)
